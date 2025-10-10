79b4cd123614251159d1ff41e83544e1d3588e9c
razzaksr <razzaksr@gmail.com>
2025-10-10 14:58:35 +0530

    standalone CRUD done with jsonserver

--- phase4/explore-ang/src/app/dashboard/dashboard.ts
+++ phase4/explore-ang/src/app/dashboard/dashboard.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Challanservice } from '../services/challanservice';
 import { Challan } from '../models/challan';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [ReactiveFormsModule,FormsModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
@@ -12,16 +13,50 @@ export class Dashboard implements OnInit {
 
   tempChallans:Challan[] = []
 
+  editId: string|null=null
+
+  challanForm!:FormGroup
+
   // dependency injection
-  constructor(private serv:Challanservice){}
+  constructor(private serv:Challanservice, private builder:FormBuilder){}
+
+  configureForm(){
+    this.challanForm = this.builder.group({
+      id: ['',[Validators.required,Validators.minLength(5)]],
+      district:['',Validators.minLength(3)],
+      vehicle:['',[Validators.required,Validators.minLength(8)]],
+      fine:[0,[Validators.required,Validators.min(1)]]
+    })
+  }
+
+  generating(){
+    if(this.challanForm.invalid)return
+    const colleted = this.challanForm.value
+    if(this.editId!=null){
+      this.serv.updating(this.editId,colleted).subscribe(()=>this.loading())
+      this.editId=null
+    }
+    else{
+      this.serv.creating(colleted).subscribe(()=>this.loading())
+    }
+    this.challanForm.reset()
+  }
+
   loading(){
     this.serv.getAll().subscribe(objs=>this.tempChallans=objs)
   }
   ngOnInit(): void {
     this.loading()
+    this.configureForm()
   }
   onRemoveCalled(id:string){
     this.serv.removeViaId(id).subscribe(()=>this.loading())
   }
 
+  modify(id:string){
+    this.editId=id
+    const found = this.tempChallans.filter(obj=>obj.id==id)[0]
+    this.challanForm.patchValue(found)
+    this.loading()
+  }
 }
--- phase4/explore-ang/src/app/services/challanservice.ts
+++ phase4/explore-ang/src/app/services/challanservice.ts
@@ -15,4 +15,10 @@ export class Challanservice {
   removeViaId(id:string):Observable<void>{
     return this.client.delete<void>(`${this.dataUrl}/${id}`)
   }
+  creating(object:Challan):Observable<Challan>{
+    return this.client.post<Challan>(this.dataUrl,object)
+  }
+  updating(id:string,obj:Challan):Observable<Challan>{
+    return this.client.put<Challan>(`${this.dataUrl}/${id}`,obj)
+  }
 }
